18f171da91707228abe583660f6b2e56dd3c497d
foul1one <foul1one@gmail.com>
2023-01-22 16:08:03 +0400

    Сделал форму авторизации

--- src/main/java/com/vladislav/fedoseev/SpringSecurityBasicAuthFormAuthJWT/config/SecurityConfig.java
+++ src/main/java/com/vladislav/fedoseev/SpringSecurityBasicAuthFormAuthJWT/config/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration // помечаем что класс является конфигурационным
