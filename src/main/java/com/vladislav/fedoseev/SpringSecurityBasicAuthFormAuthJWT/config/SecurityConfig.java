9aaed5c1e30faa82b816e0624d98148eb4854251
foul1one <foul1one@gmail.com>
2023-01-22 13:11:09 +0400

    Добавил права доступа для ролей

--- src/main/java/com/vladislav/fedoseev/SpringSecurityBasicAuthFormAuthJWT/config/SecurityConfig.java
+++ src/main/java/com/vladislav/fedoseev/SpringSecurityBasicAuthFormAuthJWT/config/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.vladislav.fedoseev.SpringSecurityBasicAuthFormAuthJWT.config;
 
-import com.vladislav.fedoseev.SpringSecurityBasicAuthFormAuthJWT.model.Permission;
 import com.vladislav.fedoseev.SpringSecurityBasicAuthFormAuthJWT.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
